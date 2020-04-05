b39de4c7271170ea3d266ad02bd48d008033bc7d
Ivana <croxcatto.maria.ivana@gmail.com>
2020-04-05 16:09:28 -0300

    prender y apagar la luz con styled-components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import styled from 'styled-components';
 
-function App() {
+const Div = styled.div`
+  height: 600px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props => props.clase ? "#EDEDA5" : "#292A2A"}; 
+      
+    button {
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+      background-color: black;
+      color: white;
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+      font-weight: bold;
+      padding: 25px;
+      box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+    }
+`;
+
+const App = () => {
+
+  const [luzPrendida, setluzPrendida] = useState(false)
+  
+  const handleClick = () => {
+    setluzPrendida(!luzPrendida)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div clase={luzPrendida}>
+      <button onClick={handleClick}>{!luzPrendida ? "Prender la luz" : "Apagar la luz"}</button>
+    </Div>
   );
 }
 
